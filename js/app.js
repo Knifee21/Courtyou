b23f4d9a0c375df14bb90f82a0f278f1c952ac2f
Knifee21 <micodioquino7@gmail.com>
2025-03-28 19:59:31 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+const gif = document.querySelector("#gif");
+const text = document.querySelector("#text");
+const vid = document.querySelector("video");
+let count = 2;
+
+const gifs = [
+  "cat-heart.gif",
+  "rusure.gif",
+  "3shocked-1.gif",
+  "4.crying.gif",
+  "5.crying.gif",
+  "idc.gif"
+];
+
+gifs.forEach(gifSrc => {
+  const img = new Image();
+  img.src = gifSrc;
+});
+
+no.addEventListener("click", () => {
+  if (count == 2) {
+    gif.src = "rusure.gif";
+    text.innerHTML = "You meant to press YES right?🤨";
+    yes.style.height = "65%";
+    yes.style.width = "60%";
+    no.style.width = "30%";
+    count++;
+  } else if (count == 3) {
+    gif.src = "3shocked-1.gif";
+    text.innerHTML = "Your hand must have slipped right?🥹";
+    yes.style.height = "70%";
+    yes.style.width = "70%";
+    no.style.width = "20%";
+    count++;
+  } else if (count == 4) {
+    gif.src = "4.crying.gif";
+    text.innerHTML = "I'm gonna cry😭";
+    yes.style.height = "80%";
+    yes.style.width = "80%";
+    no.style.fontSize = "4vh";
+    no.style.width = "10%";
+    count++;
+  } else if (count == 5) {
+    gif.src = "5.crying.gif";
+    text.innerHTML = "Pretty Please🥺😘";
+    yes.style.height = "90%";
+    yes.style.width = "96%";
+    no.style.display = "none";
+  }
+});
+
+yes.addEventListener("click", () => {
+  vid.style.display = "block";
+  gif.src = "idc.gif";
+  text.innerHTML = "YEYYYYY!😘";
+  yes.innerHTML = '<a href="https://www.instagram.com/frank_mic21/">Message me</a>';
+  yes.style.height = "90%";
+  yes.style.width = "96%";
+  no.style.display = "none";
+  setTimeout(() => {
+    vid.style.display = "none";
+  }, 9000);
+});
